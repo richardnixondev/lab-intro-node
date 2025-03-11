a2f2ba47939fbc48b4f796cb6e8464fd6c6bb277
Richard Nixon <rnixonaf@gmail.com>
2025-03-11 21:55:56 +0000

    labs intro node finished 5 interations + 2 bonus

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item); 
+    this.length = this.items.length; 
+    this.items.sort((a, b) => a - b);
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+   if (this.items[pos] > 0){
+     return this.items[pos];
+   }else{
+    throw new Error("OutOfBounds");
+   }     
+}
 
-  min() {}
 
-  sum() {}
+  max() {
+    if (this.items.length > 0){
+      const highest = Math.max(...this.items);
+      return highest;
+    }else{
+     throw new Error("OutOfBounds");
+    }  
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length > 0){
+      const highest = Math.min(...this.items);
+      return highest;
+    }else{
+     throw new Error("OutOfBounds");
+    }  
+  }
+
+  sum() {
+    if (this.items.length > 0){
+      const sum = this.items.reduce((acc, curr) => acc + curr, 0);
+      return sum;
+  } else {
+      return 0;
+  }
+}
+
+  avg() {
+    if (this.items.length === 0){
+      throw new Error("OutOfBounds");
+    }
+    const sum = this.items.reduce((acc, curr) => acc + curr, 0);
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
